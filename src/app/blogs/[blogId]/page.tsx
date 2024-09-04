8de6d6e063081cd75e64b5d9c92e9b45268b554c
nargis21 <nargisakther1933@gmail.com>
2024-09-04 15:20:45 +0600

    ssg added

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,7 +1,16 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { TBlog } from "@/types";
 
 type TBlogId = {
     blogId: string
+}
+
+export const generateStaticParams = async () => {
+    const res = await fetch('http://localhost:5000/blogs')
+    const blogs = await res.json()
+    return blogs.slice(0, 3).map((blog: TBlog) => ({
+        blogId: blog.id
+    }))
 }
 
 const BlogDetailsPage = async ({ params }: { params: TBlogId }) => {
