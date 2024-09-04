3057224cdb1dd218b1e0f7eeadeb546ef26584ac
nargis21 <nargisakther1933@gmail.com>
2024-09-04 12:07:53 +0600

    revalidattion added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import LatestBlogs from "@/components/latestBlogs/LatestBlogs";
 
 const HomePage = async () => {
 
-  const res = await fetch('http://localhost:5000/blogs')
+  const res = await fetch('http://localhost:5000/blogs', {
+    next: {
+      revalidate: 30
+    }
+  })
   const blogs = await res.json()
   return (
     <>
